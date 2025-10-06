6dbd6ab9afe546a975bebfa0fa0dcc8c11f9226c
Elliot O'Toole <elliototoole@gmail.com>
2025-10-06 10:57:32 +1000

    Truck-kun

--- Assets/Scripts/VehicleSpawner.cs
+++ Assets/Scripts/VehicleSpawner.cs
@@ -5,6 +5,7 @@ public class VehicleSpawner : MonoBehaviour
     [SerializeField] GameObject[] vehiclePrefabs;
     [SerializeField] Transform[] vehicleSpawnPoints;
     [SerializeField] float vehicleSpawnRate = 5f;
+    [SerializeField] GameObject txt_TruckKun;
     [Range(1, 100)]
     public int TruckKunSpawnChance = 10; //Percentage from 1 to 100 as a whole number
     private void Start()
@@ -19,10 +20,11 @@ public class VehicleSpawner : MonoBehaviour
         int RandomVehicleIndex = Random.Range(0, vehiclePrefabs.Length);
         int RandomSpawnPointIndex = Random.Range(0, vehicleSpawnPoints.Length);
         GameObject carObject = Instantiate(vehiclePrefabs[RandomVehicleIndex], vehicleSpawnPoints[RandomSpawnPointIndex].position, Quaternion.identity, transform);
-        if(Random.Range(1, 100) <= vehicleSpawnRate)
+        if(Random.Range(1, 100) <= TruckKunSpawnChance)
         {
             Debug.Log("Spawning TruckKun");
-            carObject.transform.localScale = Vector3.one * 4;
+            Instantiate(txt_TruckKun, carObject.transform);
+            //carObject.transform.localScale = Vector3.one * 4;
         }
     }
 }
